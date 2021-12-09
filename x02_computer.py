cc099295c454db36d4a1745b112543e9845d5114
hungrybeagle <cyang@deltalearns.ca>
2021-12-09 10:08:16 -0800

    Update x02_computer.py

--- x02_computer.py
+++ x02_computer.py
@@ -1,22 +1,35 @@
 #!python3
 """
-Create a function that creates a random choice for the computer player:
-input parameters: none required
-output:
+**** The Discriminant
+The square root part is called the "discriminant".  
+1. Depending on the coefficients, it is possible for it to have a negative value. Since you can't square root a negative, when the discrminant is 0, there are no solutions to the equation.
+2. If the discriminant is 0, there is only 1 solution, because the quadratic is a perfect square
+3. If the discriminant is positive, there will be 2 solutions
+4. If the discriminant is a perfect square, then the roots will be rational numbers (fractions) and it is possible to solve the quadratic by factoring rather than having to rely on the quadratic formula
+5. If the discriminant is not a perfect square, then the roots will be irrational numbers (involving roots) and the quadratic can not be factored.
 
-0 : rock
-1 : paper
-2 : scissors
+Assignments:
+##### x02. Determine the number of solutions
+Determine the number of solutions. You will need to make use of the discriminant.  If you have already completed x01, you can import that function and make use of it in this assignment
 """
 
-def computerChoice():
+def numSolutions(discriminant):
+  """
+  input parameters:
+  discriminant: signed float
   
-  
-  
-  
-  return value
+  alternately, you can change the function definition to calculate the discriminant in the function itself:
+def numSolutions(a,b,c):
 
+  return: 
+  integer for number of solutions.  It should be 0, 1 or 2
+  """
+  
+  return None
+
+def main():
+  pass
 
 if __name__ == "__main__":
-  computer = computerChoice()
-  print(computer)
+  main()
+  
