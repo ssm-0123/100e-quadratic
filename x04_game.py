c750fe6cf4c75fd1f2a9a28d66b009cdca8940a6
hungrybeagle <cyang@deltalearns.ca>
2021-12-09 09:51:12 -0800

    Initial commit

---
+++ x04_game.py
@@ -0,0 +1,14 @@
+#!python3
+
+"""
+Create a full rock paper scissors game
+You can incorporate the other files in this project to help you
+"""
+
+from x01_player import *
+from x02_computer import *
+from x03_winner import *
+
+if __name__ == "__main__":
+  pass
+
